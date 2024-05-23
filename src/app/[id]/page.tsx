4431cbd720ca42390b7462c524ff98135a1e4cd5
kod-python <kelvinlik0245@gmail.com>
2024-05-23 16:25:00 +0000

    second commit

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { TDrink } from '@/components/ResultPage/Result';
-import Link from 'next/link';
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { TDrink } from "@/components/ResultPage/Result";
+import Link from "next/link";
 import { FaChevronLeft } from "react-icons/fa";
 
 const DetailPage = ({ params }: { params: { id: string } }) => {
@@ -9,7 +11,9 @@ const DetailPage = ({ params }: { params: { id: string } }) => {
   useEffect(() => {
     const getDrinks = async () => {
       try {
-        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${params.id}`);
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${params.id}`
+        );
         const data = await response.json();
         setDrinks(data.drinks);
       } catch (error) {
@@ -21,24 +25,27 @@ const DetailPage = ({ params }: { params: { id: string } }) => {
   }, [params.id]);
 
   return (
-    <div className='w-[1000px] mx-auto p-10'>
-      <section className='text-3xl font-bold underline mb-10 flex justify-center space-x-3'>
+    <div className="w-[1000px] mx-auto p-10">
+      <section className="text-3xl font-bold underline mb-10 flex justify-center space-x-3">
         <Link href="/">
-          <span><FaChevronLeft /></span>
+          <span>
+            <FaChevronLeft />
+          </span>
         </Link>
         <h1>Champagne Detail Page</h1>
       </section>
 
-      {drinks && drinks.map((drink) => (
-        <section key={drink.idDrink}>
-          <img src={drink.strDrinkThumb} alt="drink-logo" />
-          <h2>{drink.strDrink}</h2>
-          <p>{drink.strCategory}</p>
-          <p>{drink.strInstructions}</p>
-        </section>
-      ))}
+      {drinks &&
+        drinks.map((drink) => (
+          <section key={drink.idDrink}>
+            <img src={drink.strDrinkThumb} alt="drink-logo" />
+            <h2>{drink.strDrink}</h2>
+            <p>{drink.strCategory}</p>
+            <p>{drink.strInstructions}</p>
+          </section>
+        ))}
     </div>
   );
-}
+};
 
 export default DetailPage;
